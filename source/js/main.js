e3d13c46c599b5b8c6ba527f13504b6010bdb562
KeusVult <kacpi.von.zapart@gmail.com>
2024-04-01 12:51:48 +0200

    kij wie

--- source/js/main.js
+++ source/js/main.js
@@ -3,3 +3,14 @@ function Redirect(a) {
         location.replace("register.html"); 
     }
 }
+var a = true;
+function Callendar() {
+    if(a==true) {
+        document.getElementById('firstCalendar').style.display = "none";
+        document.getElementById('secondCalendar').style.display = "none";
+        a=false;
+    }else if(a==false) {
+        document.getElementById('firstCalendar').style.display = "block";
+        document.getElementById('secondCalendar').style.display = "block";
+    }
+}
