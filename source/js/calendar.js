11f7479592985135c9cf6caa2a54cd8bcb2b7de3
KeusVult <kacpi.von.zapart@gmail.com>
2024-08-04 11:43:32 +0200

    Bug with Sunday

--- source/js/calendar.js
+++ source/js/calendar.js
@@ -1,5 +1,8 @@
 function createFirstCalendar(month, year) {
     let firstDay = new Date(year, month, 1).getDay()-1;
+    if(firstDay == (-1)) {
+        firstDay = 6;
+    }
     let daysInMonth = new Date(year, month + 1, 0).getDate();
     let calendarHtml = '<table>';
     calendarHtml += '<tr><th>PO</th><th>WT</th><th>ŚR</th><th>CZ</th><th>PT</th><th>SO</th><th>NI</th></tr><tr>';
@@ -22,6 +25,9 @@ function createSecondCalendar(month,year) {
         year += 1;
     }
     let firstDay = new Date(year, month + 1, 1).getDay()-1;
+    if(firstDay == (-1)) {
+        firstDay = 6;
+    }
     let daysInMonth = new Date(year, month + 2, 0).getDate();
     let calendarHtml = '<table>';
     calendarHtml += '<tr><th>PO</th><th>WT</th><th>ŚR</th><th>CZ</th><th>PT</th><th>SO</th><th>NI</th></tr><tr>';
@@ -39,12 +45,12 @@ function createSecondCalendar(month,year) {
     return calendarHtml;
 }
 function Data(month, year) {
-    const months = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Październik', 'Listopad', 'Grudzień'];
+    const months = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec','Sierpień','Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
     document.getElementById("firstDate").innerHTML = months[month] + " " + year;
-    if(month == 11){
-        year += 1;
+    if(month == 11) {
         month = 0;
-        document.getElementById("secondDate").innerHTML = months[month] + " " + year;
+        year += 1
+        document.getElementById("secondDate").innerHTML = months[month]
     } else
         document.getElementById("secondDate").innerHTML = months[month+1] + " " + year;
 }
